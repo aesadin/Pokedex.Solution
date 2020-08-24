3758b6a59bc349b4d7161d466dc9ea15f6243461
Tristan Emmerson <tristan@stickerslug.com>
2020-08-24 14:22:04 -0700

    populate controller with get for API call
    Co-authored-by: James Henager <jameschenager@gmail.com>
    Co-authored-by: Taylor Phillips <TaylorPhillips133@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- Pokedex/Controllers/PokemonController.cs
+++ Pokedex/Controllers/PokemonController.cs
@@ -1,21 +1,28 @@
-﻿using Pokedex.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
 
 namespace Pokedex.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PokemonController : ControllerBase
+    public class PokemonsController : ControllerBase
     {
+        private PokedexContext _db;
+
+        public PokemonController(PokedexContext db)
+        {
+            _db = db;
+        }
+
         // GET api/pokemon
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Pokemon>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _db.Pokemon.ToList();
         }
 
         // GET api/pokemon/5
@@ -25,22 +32,22 @@ namespace Pokedex.Controllers
             return "value";
         }
 
-        // POST api/pokemon
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        // // POST api/pokemon
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+        // }
 
-        // PUT api/pokemon/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        // // PUT api/pokemon/5
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody] string value)
+        // {
+        // }
 
-        // DELETE api/pokemon/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        // // DELETE api/pokemon/5
+        // [HttpDelete("{id}")]
+        // public void Delete(int id)
+        // {
+        // }
     }
 }
---
+++ Pokedex/Controllers/TypesController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+
+namespace Pokedex.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesController : ControllerBase
+    {
+      private PokedexContext _db;
+
+      public TypesController(PokedexContext db)
+      {
+        _db = db;
+      }
+
+      // GET api/type
+      [HttpGet]
+      public ActionResult<IEnumerable<Type>> Get()
+      {
+        return _db.Type.ToList();
+      }
+
+      // GET api/type/5
+      [HttpGet("{id}")]
+      public ActionResult<Type> Get(int id)
+      {
+        return _db.Type.FirstOrDefault(entry => entry.TypeId == id);
+      }
+    }
+}
