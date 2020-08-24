1c845e4367abf406096fd4fae147371811e0aed4
Tristan Emmerson <tristan@stickerslug.com>
2020-08-24 14:00:41 -0700

    test

--- Pokedex/Controllers/PokemonController.cs
+++ Pokedex/Controllers/PokemonController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pokedex.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,35 +9,35 @@ namespace Pokedex.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PokemonController : ControllerBase
     {
-        // GET api/values
+        // GET api/pokemon
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/pokemon/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/pokemon
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/pokemon/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/pokemon/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- Pokedex/Models/PokedexContext.cs
+++ Pokedex/Models/PokedexContext.cs
@@ -9,7 +9,7 @@ namespace Pokedex.Models
     }
     public DbSet<User> Users { get; set; }
     public DbSet<Pokemon> Pokemon { get; set; }
-    public DbSet<Types> Types { get; set; }
+    public DbSet<Type> Types { get; set; }
     public DbSet<PokemonType> PokemonTypes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
@@ -17,7 +17,7 @@ namespace Pokedex.Models
       builder.Entity<Pokemon>()
       .HasData(
         new Pokemon { PokemonId = 1, TypeId =1 , Species = "Bulbasaur", Description = "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.", Weight = 6.9, Height = 0.7, ImageMain = "https://coding-assets.s3-us-west-2.amazonaws.com/pokemon/fight/bulbasaur.png", ImageSprite = "https://coding-assets.s3-us-west-2.amazonaws.com/pokemon/sprite/bulbasaur.gif", ImageSilhouette = "https://coding-assets.s3-us-west-2.amazonaws.com/pokemon/silhouette/bulbasaur.png", Hp = 45, Attack = 49, Defense = 49, SpAtk = 65, SpDef = 65, Speed = 45},
-      )
+      );
     }
   }
 }
