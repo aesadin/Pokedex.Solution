ff3c506fffdf7a7354e1c34536b71800aac9ae79
Tristan Emmerson <tristan@stickerslug.com>
2020-08-24 16:59:10 -0700

    EOD.

--- Pokedex/Controllers/PokeTypesController.cs
+++ Pokedex/Controllers/PokeTypesController.cs
@@ -33,7 +33,22 @@ namespace Pokedex.Controllers
         [HttpGet("{id}")]
         public ActionResult<PokeType> Get(int id)
         {
-          return _db.PokeTypes.FirstOrDefault(entry => entry.PokeTypeId == id);
+          var allInfo = _db.PokeTypes
+          .Include(pokeType => pokeType.Pokemon)
+          .FirstOrDefault(entry => entry.PokeTypeId == id);
+          return allInfo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PokeType>> Get(string pokeTypeName)
+        {
+          var query = _db.PokeTypes.AsQueryable();
+          if (pokeTypeName != null)
+          {
+            query = query.Where(entry => entry.PokeTypeName == pokeTypeName);
+          }
+
+          return query.ToList();
         }
     }
 }
