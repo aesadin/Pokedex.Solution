6877ab72a33bc1fb8532db4c392b49e62818d0d3
Tristan Emmerson <tristan@stickerslug.com>
2020-08-24 15:27:34 -0700

    update user model

--- Pokedex/Models/User.cs
+++ Pokedex/Models/User.cs
@@ -1,9 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Pokedex.Models
 {
   public class User
   {
     public int UserId { get; set; }
     public string Name { get; set; }
+    public
     public 
   }
 }
