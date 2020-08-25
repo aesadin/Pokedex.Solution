45f3d7edbeadc0e7a94e14e4c6bc7b24769ee73c
James Henager <jameschenager@gmail.com>
2020-08-25 15:39:24 -0700

    add pokemon querying and pagination
    Co-authored-by: Taylor Phillips <taylorphillips133@gmail.com>
    Co-authored-by: aesadin <aesadin@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>

--- Pokedex/Controllers/PokemonController.cs
+++ Pokedex/Controllers/PokemonController.cs
@@ -49,5 +49,70 @@ namespace Pokedex.Controllers
         // public void Delete(int id)
         // {
         // }
+
+
+        //Querying
+        [HttpGet]
+        public ActionResult<IEnumerable<Pokemon>> Get(string Species, string Description, double Weight, double Height, int Hp, int Attack, int Defense, int SpAtk, int SpDef, int Speed, string Ability)
+        {
+            var query = _db.Pokemon.AsQueryable();
+            if(Species != null)
+            {
+                query = query.Where(entry => entry.Species == Species);
+            }
+            if(Description != null)
+            {
+                query = query.Where(entry => entry.Description == Description);
+            }
+            if(Weight != null)
+            {
+                query = query.Where(entry => entry.Weight == Weight);
+            }
+            if(Height != null)
+            {
+                query = query.Where(entry => entry.Height == Height);
+            }
+            if(Hp != null)
+            {
+                query = query.Where(entry => entry.Hp == Hp);
+            }
+            if(Attack != null)
+            {
+                query = query.Where(entry => entry.Attack == Attack);
+            }
+            if(Defense != null)
+            {
+                query = query.Where(entry => entry.Defense == Defense);
+            }
+            if(SpAtk != null)
+            {
+                query = query.Where(entry => entry.SpAtk == SpAtk);
+            }
+            if(SpDef != null)
+            {
+                query = query.Where(entry => entry.SpDef == SpDef);
+            }
+            if(Speed != null)
+            {
+                query = query.Where(entry => entry.Speed == Speed);
+            }
+            if(Ability != null)
+            {
+                query = query.Where(entry => entry.Ability == Ability);
+            }
+            return query.ToList();
+        }
+
+        //Pagination
+        [HttpGet ("page")]
+        public ActionResult<Pokemon> GetPage([FromQuery] UrlQuery urlQuery)
+        {
+            var valudUrlQuery = new UrlQuery(urlQuery.PageNumber, urlQuery.PageSize);
+            var pagedData = _db.Pokemon
+                .OrderBy(pokemon => pokemon.PokemonId)
+                .Skip((valueUrlQuery.PageNumber-1) * validUrlQuery.PageSize)
+                .Take(validUrlQuery.PageSize);
+            return Ok(pagesData);
+        }
     }
 }
